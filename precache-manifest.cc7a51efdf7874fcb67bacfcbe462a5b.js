2f126aaa911194a03275b899a8391a689ba6efb8
kitarp29 <56778773+kitarp29@users.noreply.github.com>
2020-09-29 23:51:16 +0530

    Updates

---
+++ precache-manifest.cc7a51efdf7874fcb67bacfcbe462a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb36d6467876948458e1fd18f33d885",
+    "url": "/chatpass/index.html"
+  },
+  {
+    "revision": "3488a54ae532c050c3b9",
+    "url": "/chatpass/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8d344991e65bbb5fc1c",
+    "url": "/chatpass/static/js/2.a47923bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chatpass/static/js/2.a47923bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3488a54ae532c050c3b9",
+    "url": "/chatpass/static/js/main.0a8a0f5c.chunk.js"
+  },
+  {
+    "revision": "e0b7e0455455ef64de8f",
+    "url": "/chatpass/static/js/runtime-main.a2c4abcc.js"
+  }
+]);
